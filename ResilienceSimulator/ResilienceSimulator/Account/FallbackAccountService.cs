e7aa10d69dbf86c5221e37edb4f60fed1b2c2e7a
Formanek Gábor <formanek.gabor@gmail.com>
2021-07-01 16:40:08 +0200

    fallback

---
+++ ResilienceSimulator/ResilienceSimulator/Account/FallbackAccountService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Fallback;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResilienceSimulator.Account
+{
+    public class FallbackAccountService : AccountService, IAccountService
+    {
+        public FallbackAccountService(
+            ILogger<TimeoutAccountService> logger) : base(logger) { }
+
+        private AsyncFallbackPolicy ResilientStrategy =>
+            Policy
+            .Handle<AccountException>()
+            .FallbackAsync(async (ct) => await GetCurrentBalanceFallbackAsync(ct));
+
+        public async Task<long> GetCurrentBalanceAsync(CancellationToken cancellationToken = default)
+        {
+            return await ResilientStrategy.ExecuteAsync(async (ct) => await GetCurrentBalanceFromBackendAsyncThrow(ct), CancellationToken.None);
+        }
+
+        private async Task<long> GetCurrentBalanceFallbackAsync(CancellationToken cancellationToken = default)
+        {
+            return await Task.FromResult(100L);
+        }
+    }
+}
