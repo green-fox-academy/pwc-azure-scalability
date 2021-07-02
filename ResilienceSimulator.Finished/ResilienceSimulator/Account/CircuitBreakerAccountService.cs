1c38fce8ac747245b42af67c7d96e87b1d0de857
Formanek Gábor <formanek.gabor@gmail.com>
2021-07-02 07:36:20 +0200

    circuit breaker

--- ResilienceSimulator.Finished/ResilienceSimulator/Account/CircuitBreakerAccountService.cs
+++ ResilienceSimulator.Finished/ResilienceSimulator/Account/CircuitBreakerAccountService.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using Polly;
 using Polly.CircuitBreaker;
+using Polly.Retry;
+using Polly.Timeout;
+using Polly.Wrap;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,12 +13,38 @@ namespace ResilienceSimulator.Account
     public class CircuitBreakerAccountService : AccountService, IAccountService
     {
         public CircuitBreakerAccountService(
-            ILogger<RetryAccountService> logger) : base(logger) { }
+            ILogger<RetryAccountService> logger) : base(logger)
+        {
+            ResilientStrategy = Policy.WrapAsync(RetryResilientStrategy, CircuitBreakerResilientStrategy);
+        }
 
-        private AsyncCircuitBreakerPolicy ResilientStrategy =>
+        private AsyncCircuitBreakerPolicy CircuitBreakerResilientStrategy =>
             Policy
             .Handle<AccountException>()
-            .CircuitBreakerAsync(2, TimeSpan.FromSeconds(10));
+            .Or<TimeoutRejectedException>()
+            .CircuitBreakerAsync(
+                exceptionsAllowedBeforeBreaking: 2,
+                durationOfBreak: TimeSpan.FromMilliseconds(1000),
+                onBreak: (e, ts) => _logger.LogInformation($"Closed->Open {e.GetType().Name} - {ts.ToString()}"),
+                onReset: () => _logger.LogInformation("Open->Closed"),
+                onHalfOpen: () => _logger.LogInformation("Half open"));
+
+        private AsyncRetryPolicy RetryResilientStrategy =>
+            Policy
+            .Handle<AccountException>()
+            .Or<BrokenCircuitException>()
+            .Or<TimeoutRejectedException>()
+            .WaitAndRetryAsync(
+                retryCount: 10,
+                sleepDurationProvider: (tn) => TimeSpan.FromMilliseconds(200),
+                onRetryAsync: (e, ts) =>
+                {
+                    _logger.LogInformation($"Retry: {e.GetType().Name} - {ts.ToString()}");
+
+                    return Task.CompletedTask;
+                });
+
+        AsyncPolicyWrap ResilientStrategy { get; set; }
 
         public async Task<long> GetCurrentBalanceAsync(CancellationToken cancellationToken = default)
         {
