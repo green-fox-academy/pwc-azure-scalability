ad5e19090a6497c7d96e5c25d45bf77ac49d8df9
Formanek Gábor <formanek.gabor@gmail.com>
2021-07-03 16:33:36 +0200

    Push model refresh sample

--- ConfigurationManagement/ConfigurationManagement/Program.cs
+++ ConfigurationManagement/ConfigurationManagement/Program.cs
@@ -1,3 +1,5 @@
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
@@ -6,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConfigurationManagement
@@ -13,6 +16,11 @@ namespace ConfigurationManagement
     public class Program
     {
         private static string appConfigurationConnectionString = "Endpoint=https://appcs-fg-pwc.azconfig.io;Id=pi5x-l9-s0:SZLlhHyJ9Nz2MpAl04cU;Secret=CQ+mlfQqkzfZv4XMMxgigJ/seeXMKwNsqW/rM3wmtuE=";
+        private static string queueConnectionString = "DefaultEndpointsProtocol=https;AccountName=stgfevents;AccountKey=59/PEl9dxJTMrRlH7J4huOcHC5BXeN6czh9CIN240+NGOHZYed4qYqRz1IZD3ef9dZGwjUqHlNKyERi+wihl7Q==;EndpointSuffix=core.windows.net";
+        private static string queueName = "configchanges";
+
+        private static IConfigurationRefresher _refresher = null;
+        private static Timer _refresherTimer;
 
         public static void Main(string[] args)
         {
@@ -30,6 +38,11 @@ namespace ConfigurationManagement
                             appConfigOptions.Connect(appConfigurationConnectionString);
                             //appConfigOptions.Select(KeyFilter.Any, LabelFilter.Null);
                             appConfigOptions.Select(KeyFilter.Any, "Stage"); //context.HostingEnvironment.EnvironmentName
+
+                            _refresher = appConfigOptions.GetRefresher();
+
+                            //RegisterRefreshEventHandler();
+
                             appConfigOptions.ConfigureRefresh(appConfigRefresherOption =>
                             {
                                 appConfigRefresherOption.SetCacheExpiration(TimeSpan.FromSeconds(2));
@@ -37,12 +50,33 @@ namespace ConfigurationManagement
                             });
                             appConfigOptions.UseFeatureFlags(featureFlagOptions =>
                             {
-                                //featureFlagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(1);
+                                featureFlagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(1);
                                 featureFlagOptions.Select(KeyFilter.Any, LabelFilter.Null);
                             });
                         }, false);
                     });
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void RegisterRefreshEventHandler()
+        {
+            _refresherTimer = new Timer(RefreshFromQueue, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+        }
+
+        private static void RefreshFromQueue(object state)
+        {
+            var queueClient = new QueueClient(queueConnectionString, queueName);
+            var receivedMessages = queueClient.ReceiveMessages();
+
+            if (receivedMessages.Value.Count() > 0)
+            {
+                _refresher.SetDirty(TimeSpan.FromSeconds(1));
+            }
+
+            foreach (QueueMessage message in receivedMessages.Value)
+            {
+                queueClient.DeleteMessage(message.MessageId, message.PopReceipt);
+            }
+        }
     }
 }
