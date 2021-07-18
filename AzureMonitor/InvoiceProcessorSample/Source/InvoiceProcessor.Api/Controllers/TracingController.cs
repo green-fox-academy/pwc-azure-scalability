beed9a3f82b8f222956c123fa4b6bd981d8aa501
Papp, Tamás <tpapp@itron.com>
2021-07-18 22:26:23 +0200

    .

--- AzureMonitor/InvoiceProcessorSample/Source/InvoiceProcessor.Api/Controllers/TracingController.cs
+++ AzureMonitor/InvoiceProcessorSample/Source/InvoiceProcessor.Api/Controllers/TracingController.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
@@ -56,6 +60,39 @@ namespace InvoiceProcessor.Api.Controllers
             await Task.Delay(500);
             _telemetryClient.StopOperation(dependencyOperation);
             await Task.Delay(500);
+            _telemetryClient.StopOperation(requestOperation);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route(nameof(Sample3))]
+        public async Task<ActionResult> Sample3()
+        {
+            var requestActivity = new Activity(nameof(Sample2));
+            var requestOperation = _telemetryClient.StartOperation<DependencyTelemetry>(requestActivity);
+            var random = new Random();
+
+#pragma warning disable CA5394 // Do not use insecure randomness
+            var delays = Enumerable.Range(1, 100).Select(_ => TimeSpan.FromSeconds(random.Next(10) * 0.5)).ToArray();
+#pragma warning restore CA5394 // Do not use insecure randomness
+
+            var tasks = new List<Task>();
+            for (var i = 0; i < delays.Length; i++)
+            {
+                var index = i;
+                var delay = delays[index];
+                tasks.Add(Task.Run(async () =>
+                {
+                    var dependencyActivity = new Activity($"#{index} dependency");
+                    var dependencyOperation = _telemetryClient.StartOperation<DependencyTelemetry>(dependencyActivity);
+
+                    await Task.Delay(delay);
+                    _telemetryClient.StopOperation(dependencyOperation);
+                }));
+            }
+
+            await Task.WhenAll(tasks);
             _telemetryClient.StopOperation(requestOperation);
 
             return Ok();
